c47eaa0e779c88468c0b3b67bc9cac9eec01bcb5
WesleyAraujoFernandes <wesley@viaapia.com.br>
2026-05-23 00:13:08 -0300

    guard service implemented

--- src/app/service/guard.service.ts
+++ src/app/service/guard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
+import { ApiService } from './api.service';
 @Injectable({
   providedIn: 'root'
 })
-export class GuardService {
+export class GuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const requiresAdmin = route.data['requiresAdmin'] || false;
+    if (requiresAdmin) {
+      if (this.apiService.isAdmin()) {
+        return true; // Authorized as admin, allow access
+      } else {
+        this.router.navigate(['/login'],{ queryParams: { returnUrl: state.url } });
+        return false; // Not authorized, redirect to login
+      }
+    } else {
+      if (this.apiService.isAuthenticated()) {
+        return true; // Authenticated, allow access
+      } else {
+        this.router.navigate(['/login'],{ queryParams: { returnUrl: state.url } });
+        return false; // Not authenticated, redirect to login
+      }
+    }
+  }
 }
