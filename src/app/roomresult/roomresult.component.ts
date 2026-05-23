bdbc3496b6693567908c9eb1a2e63d14e2fc860f
WesleyAraujoFernandes <wesley@viaapia.com.br>
2026-05-23 12:20:51 -0300

    room results component created

--- src/app/roomresult/roomresult.component.ts
+++ src/app/roomresult/roomresult.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-roomresult',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './roomresult.component.html',
   styleUrl: './roomresult.component.css'
 })
 export class RoomresultComponent {
+  @Input() roomSearchResults: any[]=[];
+  isAdmin: boolean = false;
 
+  constructor(private router: Router, private apiService: ApiService) {
+    this.isAdmin = this.apiService.isAdmin();
+  }
+
+  navigateToEditRoom(roomId: number) {
+    this.router.navigate([`/admin/edit-room/${roomId}`]);
+  }
+
+  navigateToRoomDetails(roomId: number) {
+    this.router.navigate([`/room-details/${roomId}`]);
+  }
 }
