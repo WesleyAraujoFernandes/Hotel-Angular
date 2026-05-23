cf21dcd941e0f9cf43193a9e4369d2fae4d89865
WesleyAraujoFernandes <wesley@viaapia.com.br>
2026-05-23 15:37:37 -0300

    profile page start creation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private apiService: ApiService, private router: Router) {}
+  formData: any = {
+    email: '',
+    password: '',
+  };
+  error: any = null;
 
+  async handleSubmit() {
+    if (!this.formData.email || !this.formData.password) {
+      this.showError('Please fill in all fields');
+      return;
+    }
+    this.apiService.loginUser(this.formData).subscribe({
+      next: (res) => {
+        this.apiService.encryptAndSaveToStorage('token', res.token);
+        this.apiService.encryptAndSaveToStorage('role', res.role);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.showError(err?.error?.message || 'Unable to login: '+err?.message || ' An error occurred');
+      }
+    });
+  }
+
+  showError(msg: string) {
+    this.error = msg;
+    setTimeout(() => {
+      this.error = null;
+    }, 4000);
+  }
 }
