ce6255537752ef6ad018487bc87433bb8ded79cb
WesleyAraujoFernandes <wesley@viaapia.com.br>
2026-05-23 11:20:43 -0300

    pagination created

---
+++ src/app/roomresult/roomresult.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roomresult',
+  imports: [],
+  templateUrl: './roomresult.component.html',
+  styleUrl: './roomresult.component.css'
+})
+export class RoomresultComponent {
+
+}
---
+++ src/app/roomsearch/roomsearch.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roomsearch',
+  imports: [],
+  templateUrl: './roomsearch.component.html',
+  styleUrl: './roomsearch.component.css'
+})
+export class RoomsearchComponent {
+
+}
